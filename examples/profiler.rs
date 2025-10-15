56fa218ae6242f196539d5eb8be1cf70eb36577e
Sander Saares <sander@saares.eu>
2025-10-15 15:27:08 +0300

    asdfasdf

--- examples/profiler.rs
+++ examples/profiler.rs
@@ -1,5 +1,5 @@
-/// When profiling, it is important to always use a fixed number of iterations to ensure
-/// any data sets are comparable.
+/// When profiling, it is important to always use a fixed number of iterations
+/// to that the results are comparable between runs.
 const ITERATIONS: usize = 25;
 
 fn main() {
