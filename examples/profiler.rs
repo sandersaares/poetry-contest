3f49f4bf88b816a9bdde9e18a8c6b00445a30f63
Sander Saares <sander@saares.eu>
2025-10-15 15:26:53 +0300

    asdf

--- examples/profiler.rs
+++ examples/profiler.rs
@@ -1,9 +1,9 @@
 /// When profiling, it is important to always use a fixed number of iterations to ensure
 /// any data sets are comparable.
-const ITERATIONS: usize = 100;
+const ITERATIONS: usize = 25;
 
 fn main() {
     for _ in 0..ITERATIONS {
-        poetry_contest::v07_faster_maps::solve();
+        poetry_contest::v01_simple::solve();
     }
 }
